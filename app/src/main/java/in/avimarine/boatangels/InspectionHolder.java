668c7f286dcf74025c1f7bcc21304d9b4b43b7cc
Amit Yaffe <aayaffe@gmail.com>
2017-12-23 22:30:56 +0200

    Closed #16

--- app/src/main/java/in/avimarine/boatangels/InspectionHolder.java
+++ app/src/main/java/in/avimarine/boatangels/InspectionHolder.java
@@ -1,44 +1,33 @@
 package in.avimarine.boatangels;
 
-
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TextView;
+import in.avimarine.boatangels.db.objects.Inspection;
+import java.util.Date;
 /**
  * This file is part of an
  * Avi Marine Innovations project: BoatAngels
  * first created by aayaffe on 23/12/2017.
+ * This class is used to hold an inspection for a the inspection list recycler view
  */
-import android.graphics.PorterDuff;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.RotateDrawable;
-import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import in.avimarine.boatangels.db.objects.Inspection;
-import java.util.Date;
-
 
 public class InspectionHolder extends RecyclerView.ViewHolder {
   private final TextView mDateField;
   private final TextView mTextField;
-  private final RelativeLayout mMessageContainer;
-  private final LinearLayout mMessage;
+
 
 
   public InspectionHolder(View itemView) {
     super(itemView);
     mDateField = itemView.findViewById(R.id.date_time);
     mTextField = itemView.findViewById(R.id.inspection_text);
-    mMessageContainer = itemView.findViewById(R.id.message_container);
-    mMessage = itemView.findViewById(R.id.message);
   }
 
   public void bind(Inspection i) {
-    setDate(new Date(i.inspectionTime).toString());
+    Date date = new Date(i.inspectionTime);
+    setDate(DateFormat.format("dd.MM.yyyy HH:mm", date).toString());
     setText(i.message);
   }
 
--- app/src/main/java/in/avimarine/boatangels/activities/InspectionsListActivity.java
+++ app/src/main/java/in/avimarine/boatangels/activities/InspectionsListActivity.java
@@ -1,31 +1,25 @@
 package in.avimarine.boatangels.activities;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.crash.FirebaseCrash;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
 import in.avimarine.boatangels.InspectionHolder;
 import in.avimarine.boatangels.R;
 import in.avimarine.boatangels.db.objects.Inspection;
-import java.util.List;
 
 public class InspectionsListActivity extends AppCompatActivity {
 
@@ -40,14 +34,14 @@ public class InspectionsListActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_inspections_list);
     ButterKnife.bind(this);
-    Log.d(TAG,((inspectionsRv==null)?"Is null":"is not null"));
     Intent intent = getIntent();
-    String boatName = intent.getStringExtra("BOAT_NAME");
+    String boatName = intent.getStringExtra(getString(R.string.intent_extra_boat_name));
     if (boatName == null) {
-      Toast.makeText(this,"No boat passed to Activity",Toast.LENGTH_LONG).show();
-      FirebaseCrash.report(new Exception("No boat name arrived in intent to InspectionsListActivity"));
+      Toast.makeText(this, R.string.no_boat_name_error,Toast.LENGTH_LONG).show();
+      FirebaseCrash.report(new Exception(getString(R.string.no_boat_name_error)));
       finish();
     }
+    setTitle(getString(R.string.inspection_for_title_prefix) + boatName);
     inspectionsRv.setLayoutManager(new LinearLayoutManager(this));
 
     Query query = FirebaseFirestore.getInstance()
@@ -55,31 +49,14 @@ public class InspectionsListActivity extends AppCompatActivity {
         .whereEqualTo("boatName", boatName)
         .orderBy("inspectionTime")
         .limit(50);
-//    query.addSnapshotListener(new EventListener<QuerySnapshot>() {
-//      @Override
-//      public void onEvent(@Nullable QuerySnapshot snapshot,
-//          @Nullable FirebaseFirestoreException e) {
-//        if (e != null) {
-//          // Handle error
-//          //...
-//          return;
-//        }
-//
-//        // Convert query snapshot to a list of chats
-//        List<Inspection> inspections = snapshot.toObjects(Inspection.class);
-//        Log.d(TAG,"Number of inspection = " + inspections.size());
-//
-//        // Update UI
-//        // ...
-//      }
-//    });
+
     FirestoreRecyclerOptions<Inspection> options = new FirestoreRecyclerOptions.Builder<Inspection>()
         .setQuery(query, Inspection.class)
         .build();
 
     adapter = new FirestoreRecyclerAdapter<Inspection, InspectionHolder>(options) {
       @Override
-      public void onBindViewHolder(InspectionHolder holder, int position, Inspection model) {
+      public void onBindViewHolder(@NonNull InspectionHolder holder, int position, @NonNull Inspection model) {
         holder.bind(model);
       }
 
