d8e24759d45afff2633ef332f13cc32ffc9d6905
motim6026 <moti.malka25@gmail.com>
2017-12-25 16:00:56 +0200

    add activity file for add new user

---
+++ app/src/main/java/in/avimarine/boatangels/activity_add_user.java
@@ -0,0 +1,13 @@
+package in.avimarine.boatangels;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_add_user extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_user);
+  }
+}
