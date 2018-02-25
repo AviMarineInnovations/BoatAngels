033f52249f28966759e78e0689dda27445f654fd
motim6026 <moti.malka25@gmail.com>
2018-02-25 22:39:15 +0200

    get inspection by me and Ability to edit/view in my inspection

--- app/src/main/java/in/avimarine/boatangels/activities/MyInspection.java
+++ app/src/main/java/in/avimarine/boatangels/activities/MyInspection.java
@@ -9,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -22,10 +23,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import in.avimarine.boatangels.R;
 import in.avimarine.boatangels.db.objects.Inspection;
-import java.text.SimpleDateFormat;
+import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 
@@ -35,10 +34,12 @@ public class MyInspection extends AppCompatActivity {
   private String uid = FirebaseAuth.getInstance().getUid();
   private final FirebaseFirestore db = FirebaseFirestore.getInstance();
   private List<String> arrayInspe = new ArrayList<>();
-  private HashMap<Integer, String> hashMap = new HashMap<>();
+  private SparseArray<String> hashMap = new SparseArray<>();
+  private SparseArray<String> hashMapInspeUid = new SparseArray<>();
   private static final String TAG = "INSPECTION_LIST";
   private Integer indexList = 0;
-  Context context = this;
+  private Context context = this;
+  private String inspecUid;
 
 
   @Override
@@ -62,21 +63,20 @@ public class MyInspection extends AppCompatActivity {
               for (DocumentSnapshot document : task.getResult()) {
                 Inspection inspec = document.toObject(Inspection.class);
                 if (inspec.inspectorUid.equals(uid)) {
-                  String inspeData = new SimpleDateFormat("MM/dd/yyyy")
-                      .format(new Date(inspec.inspectionTime));
-
+                  String inspeData = DateFormat.getDateInstance().format(inspec.inspectionTime);
                   arrayInspe.add(inspecBoat + inspec.boatName + "\n" +
                       inspecDate + inspeData + "\n" +
                       pointsEarned + inspec.pointsEarned);
                   Log.d(TAG, document.getId() + " => " + document.getData());
 
                   hashMap.put(indexList, inspec.boatUuid);
+                  hashMapInspeUid.put(indexList, inspec.getUuid());
                   indexList++;
                 }
               }
               listView.setAdapter(arrayAdapter);
             } else {
-              Log.d(TAG, "moti Cannot find inspection");
+              Log.d(TAG, "Cannot find inspection");
             }
 
           }
@@ -85,34 +85,36 @@ public class MyInspection extends AppCompatActivity {
       public void onItemClick(AdapterView<?> arg0, View v, int position,
           long arg3) {
 
+        inspecUid = hashMapInspeUid.get(position);
         String uid = hashMap.get(position);
         Log.d(TAG, "Name : " + uid + " " + position);
-
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
 
         // set title
-        alertDialogBuilder.setTitle("what would you like to do");
+        alertDialogBuilder.setTitle(getString(R.string.what_you_like_to_do));
 
         // set dialog message
         alertDialogBuilder
             .setCancelable(false)
-            .setPositiveButton("New Inspection",new DialogInterface.OnClickListener() {
+            .setPositiveButton(getString(R.string.new_inspection),
+                new DialogInterface.OnClickListener() {
 
-              public void onClick(DialogInterface dialog,int id) {
+                  public void onClick(DialogInterface dialog, int id) {
 
-                Intent intent = new Intent(MyInspection.this, InspectBoatActivity.class);
-                intent.putExtra(getString(R.string.intent_extra_boat_uuid), uid);
-                startActivity(intent);
-              }
-            })
-            .setNegativeButton("View Inspection",new DialogInterface.OnClickListener() {
-              public void onClick(DialogInterface dialog,int id) {
-                // if this button is clicked, just close
-                // the dialog box and do nothing
-                dialog.cancel();
-              }
-            });
+                    Intent intent = new Intent(MyInspection.this, InspectBoatActivity.class);
+                    intent.putExtra(getString(R.string.intent_extra_boat_uuid), uid);
+                    startActivity(intent);
+                  }
+                })
+            .setNegativeButton(getString(R.string.view_inspection),
+                new DialogInterface.OnClickListener() {
+                  public void onClick(DialogInterface dialog, int id) {
+                    Intent intent1 = new Intent(MyInspection.this, InspectionResultActivity.class);
+                    intent1.putExtra(getString(R.string.intent_extra_inspection_uuid), inspecUid);
+                    startActivity(intent1);
+                  }
+                });
 
         // create alert dialog
         AlertDialog alertDialog = alertDialogBuilder.create();
@@ -121,9 +123,9 @@ public class MyInspection extends AppCompatActivity {
         alertDialog.show();
       }
     });
-      }
-
   }
+
+}
 
 
 
